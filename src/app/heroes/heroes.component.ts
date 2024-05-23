e00cc17d8d7fa82abe0d6d326e671dd4031551c7
Yannick <yannick.darcillon@gmail.com>
2024-05-23 15:26:02 +0200

    inject services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,25 +1,47 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 import { NgFor,NgIf,UpperCasePipe} from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
   standalone: true,
-  imports: [NgFor,NgIf,UpperCasePipe,FormsModule]
+  imports: [NgFor,NgIf,UpperCasePipe,FormsModule, HeroDetailComponent]
 })
 
 export class HeroesComponent {
 
-  heroes = HEROES;
-
+  heroes: Hero[] = [];
+  //heroes = HEROES;
   selectedHero?: Hero;
+
+
+  constructor(private heroService :  HeroService, private messageService: MessageService)
+  {
+
+
+  }
+
+  ngOnInit(): void
+  {
+    this.getHeroes();
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+        .subscribe(heroes => this.heroes = heroes);
+  }
 
   onSelect(hero: Hero): void
   {
+    this.messageService.add('HeroesComponent : click on ' +  hero.name)
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
     this.selectedHero = hero;
   }
 
